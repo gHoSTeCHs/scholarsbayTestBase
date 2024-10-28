9cf2a4b5e8306a6e9beb1de3be2c9d868be094bf
Somadina <kingswatter@gmail.com>
2024-10-28 15:32:04 +0100

    updates

--- resources/js/Components/Mdx.tsx
+++ resources/js/Components/Mdx.tsx
@@ -4,16 +4,20 @@ import remarkMath from 'remark-math'
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeFormat from "rehype-format";
+import rehypeSlug from "rehype-slug";
 import 'katex/dist/katex.min.css'
 
 const Mdx = ({markdown}: { markdown: any }) => {
 
     return (
-        <Markdown remarkPlugins={[remarkMath, remarkGfm]} rehypePlugins={[rehypeKatex, rehypeAutolinkHeadings]}>
+        <Markdown remarkPlugins={[remarkMath, remarkGfm]}
+                  rehypePlugins={[rehypeKatex, rehypeAutolinkHeadings, rehypeFormat, rehypeSlug]}>
             {markdown}
         </Markdown>
     )
 
 }
+
 
 export default Mdx
